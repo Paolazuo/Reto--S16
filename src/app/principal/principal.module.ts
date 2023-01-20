8ecdb12f25061e9b3574da83650d313e68f754f1
Paolazuo <118744688+Paolazuo@users.noreply.github.com>
2023-01-19 23:23:53 -0500

    Add files via upload

---
+++ src/app/principal/principal.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { AppRoutingModule } from '../app-routing.module';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    LoginComponent,
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule
+  ],
+  exports: [
+    HomeComponent,
+    LoginComponent,
+    MenuComponent
+  ]
+})
+export class PrincipalModule { }
